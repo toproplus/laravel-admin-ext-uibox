6881a5c89288b4e34edde55d3668eb87725a2a3f
lee <976225283@qq.com>
2021-10-24 02:17:04 +0800

    优化

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,13 +6,13 @@
  <div class="container">
      <div class="row">
          <b class="unibox-index-title col-md-6 col-md-offset-3">
-             欢迎使用laravel-admin扩展unibox组件
+             欢迎使用laravel-admin扩展UiBox组件
          </b>
      </div>
      <div class="row">
          <div class="list-group col-md-6 col-md-offset-3">
              <a href="/<?= config('admin.route.prefix')?>/uibox/StasticsInfoBox" class="list-group-item">统计信息展示-看板</a>
-             <a href="/<?= config('admin.route.prefix')?>/uibox/StasticsInfoBox" class="list-group-item">统计信息展示-看板</a>
+             <a href="https://github.com/toproplus/laravel-admin-ext-uibox" target="_blank" class="list-group-item">Github 查看更多...</a>
          </div>
      </div>
 
